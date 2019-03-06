e5878ae4364e956821a70661f876029378e5406f
piyushbest <42518831+piyushbest@users.noreply.github.com>
2019-03-06 17:38:28 +0530

    Add files via upload

--- src/throabletnt/throabletnt.php
+++ src/throabletnt/throabletnt.php
@@ -1,1 +1,48 @@
+ <?php
 
+namespace throabletnt;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\nbt\tag\ByteTag;
+use pocketmine\nbt\tag\DoubleTag;
+use pocketmine\nbt\tag\FloatTag;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\entity;
+use pocketmine\event\Listener;
+use pocketmine\entity\Entity;
+
+class throabletnt extends PluginBase implements Listener {
+    
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+    
+     public function onInteract(PlayerInteractEvent $e){
+         $player = $e->getPlayer();
+         if($e->getItem()->getId() == 46){
+             $nbt = new CompoundTag("", [
+                            "Pos" => new ListTag("Pos", [
+                                new DoubleTag("", $player->x),
+                                new DoubleTag("", $player->y + $player->getEyeHeight()),
+                                new DoubleTag("", $player->z)
+                            ]),
+                            "Motion" => new ListTag("Motion", [
+                                new DoubleTag("", -sin($player->yaw / 180 * M_PI) * cos($player->pitch / 180 * M_PI)),
+                                new DoubleTag("", -sin($player->pitch / 180 * M_PI)),
+                                new DoubleTag("", cos($player->yaw / 180 * M_PI) * cos($player->pitch / 180 * M_PI))
+                            ]),
+                            "Rotation" => new ListTag("Rotation", [
+                                new FloatTag("", $player->yaw),
+                                new FloatTag("", $player->pitch)
+                            ]),
+                            ]);
+                        $f = 2;
+                        $tnt = Entity::createEntity("PrimedTNT", $player->chunk, $nbt, true);
+                        $tnt->setMotion($tnt->getMotion()->multiply($f));   
+                        $tnt->spawnTo($player);
+                        $player->sendMessage("Hi mom");
+       }
+    }
+}
